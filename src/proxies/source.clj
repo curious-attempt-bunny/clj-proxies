a90bbf7abd48d17fe1947c752a70705f894af398
Merlyn <curious.attempt.bunny@gmail.com>
2014-09-24 17:43:21 -0700

    Obtain all proxies, serially.

--- src/proxies/source.clj
+++ src/proxies/source.clj
@@ -3,15 +3,36 @@
 
 (def proxy-regex #"^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+)$")
 
-(defn proxies
+(defn page-proxies
     [page]
         (->>
             (enlive/select page [:li.proxy])
             (map enlive/text)
             (map #(re-matches proxy-regex %))
-            (filter identity)))
+            (filter identity)
+            (map first)))
+
+(defn pages
+    [main-page]
+    (->>
+        (enlive/select main-page [:div.table-menu :a.item])
+        (map #(get-in % [:attrs :href]))
+        (map #(clojure.string/replace % #"^\." "http://www.proxy-list.org/en"))))
+
+(defn fetch
+    [url]
+    (enlive/html-resource (java.net.URL. url)))
+
+(defn proxies
+    []
+    (let [main-page (fetch "http://www.proxy-list.org/en/index.php")]
+        (concat (page-proxies main-page)
+            (->>
+                (pages main-page)
+                (map fetch)
+                (map #(page-proxies %))
+                (apply concat)))))
 
 (defn main
     []
-    (let [main-page (enlive/html-resource (java.net.URL. "http://www.proxy-list.org/en/index.php"))]
-        (prn (proxies main-page))))
+    (println (clojure.string/join "\n" (proxies))))
